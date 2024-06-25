7870eb5aeefd9cf60de8f857611b0bfc66395cdf
joywahu <wahujoy6@gmail.com>
2024-06-25 03:08:27 +0300

    Assignment done

---
+++ file.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+
+    int num[] = [* - + / ];{
+        for (int i  = 0: i < 1, 1++);
+
+    }
+    printf(';calculation of numbers .n\');{
+    
+    scanf("%f", &num );
+
+
+
+}
+return 0;
---
+++ structure.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+   printf("enter the name of the student .\n" );
+   scanf("name");
+
+   printf("enter the roll number of the student .\n");
+   scanf("%d", number);
+
+   printf("enter the marks of the student .\n");
+   scanf("%d", marks);
+
+   return 0;
+
+}
